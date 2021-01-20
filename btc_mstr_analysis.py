49a33c4feed70d23f88812fac67b15304a89fa66
joevorbs <jvorbeck1@gmail.com>
2021-01-19 21:17:44 -0500

    cleaned up all naming conventions, switched STL functions

--- btc_mstr_analysis.py
+++ btc_mstr_analysis.py
@@ -6,10 +6,11 @@ from datetime import date
 import time
 import statsmodels.api as sm
 import statistics 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from statsmodels.tsa.seasonal import STL
+from scipy import signal
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 #Current date
 today = datetime.now().date()
@@ -18,70 +19,103 @@ today = datetime.now().date()
 btc_df = pd.read_csv("btc_mstr_data/btc/btc_data_" + str(today) + ".csv")
 mstr_df = pd.read_csv("btc_mstr_data/mstr/mstr_data_" + str(today) + ".csv")
 
-#Initialize scaler
-scaler = StandardScaler()
+#Initialize scalers
+std_scaler = StandardScaler()
+minmax = MinMaxScaler()
 
 #Append mstr price to bitcoin df to have both prices in one df
 btc_df['mstr'] = mstr_df['price']
-#Convert prices to proper type
-tester = btc_df[['price','mstr']]
-tester['mstr'] = tester['mstr'].astype(float)
-tester['price'] = tester['price'].str.replace(",","").astype(float)
-#Standardize prices
-joe = scaler.fit(tester[['price','mstr']]).transform(tester[['price','mstr']])
+
+#Convert prices to proper data type
+combined_df = btc_df[['price','mstr']]
+combined_df['mstr'] = combined_df['mstr'].astype(float)
+combined_df['price'] = combined_df['price'].str.replace(",","").astype(float)
+
+#Log transform both values due to exponential movements in both assets 
+combined_df['price'] = np.log(combined_df['price'])
+combined_df['mstr'] = np.log(combined_df['mstr'])
+
+#Standardize both values after taking their respective logs
+log_std_prices = minmax.fit(combined_df[['price','mstr']]).transform(combined_df[['price','mstr']])
 
 #Obtain hour+minute combinations and rename column
-tester2 = pd.DataFrame(joe)
-tester2['time'] = btc_df['timestamp'].astype(str).str[0:5]
-tester2.rename(columns = {0: "btc", 1: "mstr"}, inplace = True)
+log_std_prices_df = pd.DataFrame(log_std_prices)
+log_std_prices_df['time'] = btc_df['timestamp'].astype(str).str[0:5]
+log_std_prices_df.rename(columns = {0: "btc", 1: "mstr"}, inplace = True)
 
 #Univariate linear model - input is (y,x)
-results = sm.OLS(tester2["mstr"],tester2["btc"]).fit()
-results.summary()
+ols_log_std_prices = sm.OLS(log_std_prices_df["mstr"],log_std_prices_df["btc"]).fit()
+#Obtain beta value and r2 
+print("β1: " + str(ols_log_std_prices.params[0]))
+print("R^2: " + str(ols_log_std_prices.rsquared))
 
 #Plot standardized mstr & btc prices against each other - noticable patterns / trends?
 fig, ax = plt.subplots() # Create the figure and axes object
-tester2.plot(x = 'time', y = 'btc', ax = ax) 
-tester2.plot(x = 'time', y = 'mstr', ax = ax, secondary_y = True)
+log_std_prices_df.plot(x = 'time', y = 'btc', ax = ax) 
+log_std_prices_df.plot(x = 'time', y = 'mstr', ax = ax, secondary_y = False)
+plt.title("BTC and MSTR's Overlapped Price Movement\n " + "r = " + str(ols_log_std_prices.params[0]))
+plt.xlabel('\nTime (Hour-Minute)')
+plt.ylabel('\nPrice (Log Transformed & Standardized)')
 
 #Polynomial regression - comparing fit
-sns.regplot(tester2['btc'] ,tester2['mstr'], order=3)
+sns.regplot(log_std_prices_df['btc'] ,log_std_prices_df['mstr'], order=3)
 
-#Cross correlation function - any lead/lag times in terms of minutes?
-def ccf(x, y, lag_max = 100):
-        result = ss.correlate(y - np.mean(y), x - np.mean(x), method = 'direct') / (np.std(y) * np.std(x) *len(y))
-        length = (len(result) - 1) // 2
-        lo = length - lag_max
-        hi = length + (lag_max + 1)
+#Pre-whiten data for cross correlation (time series as well later on)
+#Seasonal trend decompisition using LOESS - obtain residuals for cross correlation
+stl_btc = STL(tester2['btc'], period = len(tester2['btc']), seasonal=7)
+res_btc = stl.fit()
+#fig = res.plot()
+
+stl_mstr = STL(tester2['mstr'], period = len(tester2['mstr']), seasonal=7)
+res_mstr = stl.fit()
+
+stl_btc_resid = res_btc.resid
+stl_mstr_resid = res_mstr.resid
+#fig = res.plot()
+
+#Cross correlation plot - any significant lead or lag times?
+plt.xcorr(stl_btc_resid, stl_mstr_resid, maxlags = 100)
+plt.title("Cross Correlation between BTC and MSTR")
+plt.xlabel("\nLead / Lag Times (Minutes)")
+plt.ylabel("\nCorrelation Coefficient")
+
+#Secondary cross correlation function - can see exact lead/lags
+#def ccf(x, y, lag_max = 220):
+        #result = ss.correlate(y - np.mean(y), x - np.mean(x), method = 'direct') / (np.std(y) * np.std(x) *len(y))
+        #length = (len(result) - 1) // 2
+        #lo = length - lag_max
+        #hi = length + (lag_max + 1)
                   
-        return result[lo:hi]
+        #return result[lo:hi]
+        
+#ccf_func_results = pd.DataFrame(ccf(log_std_prices_df['btc'],log_std_prices_df['mstr']), columns = ['ccf_values'])
 
-#Create a dataframe of the cross correlation values and sort them
-joey = pd.DataFrame(ccf(tester2['btc'], tester2['mstr'])).sort_values(by=0, ascending = True)
-joey.rename(columns = {0: "ccf"}, inplace = True)
-
-#Print the position of the indicator and its associated correlation coefficient - to eyeball
-for i, j in zip(joey.index, joey['ccf']):
-    print(i, j)
+#Print the lead/lags and their associated correlation coefficient
+#for i, j in zip(ccf_func_results.index, ccf_func_results['ccf_values']):
+    #print(i, j)
 
 #Plot the CCF with 100 lead and lag times (minutes)
 plt.xcorr(tester2['btc'], tester2['mstr'], maxlags = 100)
 
-#BTC movement statistics - this section will be combined 
-copy_df = tester2
-copy_df['shifted_btc'] = copy_df['btc'].shift(1)
+#BTC movement summary statistics
+#Make a copy of the intial transformed df
+btc_movement  = log_std_prices_df
 
-copy_df['difference_btc'] = copy_df['shifted_btc'] - copy_df['btc']
+#Offset the rows by 1 and subtract the value from the previous row
+btc_movement['shifted_btc'] = btc_movement['btc'].shift(1)
+btc_movement['difference_btc'] = btc_movement['shifted_btc'] - btc_movement['btc']
+btc_movement['difference_btc'] = abs(btc_movement['difference_btc'])
 
-copy_df['difference_btc'] = copy_df['difference_btc'].abs()
+#Summary stats
+avg_btc = btc_movement['difference_btc'].mean()
+max_btc = btc_movement['difference_btc'].max()
+min_btc = btc_movement['difference_btc'].min()
 
-avg_btc = copy_df['difference'].mean()
-max_btc = copy_df['difference'].max()
-min_btc = copy_df['difference'].min()
+btc_movement_summary = [avg_btc,max_btc,min_btc]
 
-print("MSTR")
-for i,j in zip(['avg','max','min'],[avg_mstr,max_mstr,min_mstr]):
-     print(i, j)
+print("BTC Summary Stats")
+for i,j in zip(['avg','max','min'], btc_movement_summary):
+     print(i + ": ", j)
 
 #MSTR movement statistics
 copy_df = tester2
